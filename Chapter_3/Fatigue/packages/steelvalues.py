d84d008ee09f8bccb72434a7b61c9edae1ad3fb6
Pedro Biel <structural.eng.biel@gmail.com>
2021-02-21 12:18:50 +0100

    Update file
    Correct docstrings.

--- Chapter_3/Fatigue/packages/steelvalues.py
+++ Chapter_3/Fatigue/packages/steelvalues.py
@@ -36,12 +36,26 @@ class SteelValues:
             'S 235': 'S 235',
             'S 275': 'S 275',
             'S 355': 'S 355'
+            }  # Data with the values to search in the DataFrame.
+        
+        self.d1 = {
+            'Fe 360': 'Fe360',
+            'Fe 430': 'Fe430',
+            'Fe 510': 'Fe510',
+            'S 235': 'Fe360',
+            'S 275': 'Fe430',
+            'S 355': 'Fe510'
             }  # Data with the values to search in the database.
         
     def get_steel_grade(self):
-        """Steel grade of the material."""
+        """Getter of the steel grade of the material."""
         
         return self.steel_grade
+    
+    def get_steel_for_db(self):
+        """Getter of the steel for de database of structural steel."""
+        
+        return self.d1[self.steel_grade]
     
     def elastic_limit(self):
         """Elastic limit fy."""
@@ -53,7 +67,7 @@ class SteelValues:
         return sigma_E
     
     def ultimate_tensile_strength(self):
-        """ultimate tensile strength fu."""
+        """Ultimate tensile strength fu."""
         
         sigma_R = self.df.loc[(
             self.df['Calidad'] == self.d[self.steel_grade]) & (
